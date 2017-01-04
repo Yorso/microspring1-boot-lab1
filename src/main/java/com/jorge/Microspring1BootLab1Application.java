64804d75fab50eab090ce6b0ea182e325c45d802
Yorso <jorgeguzon@gmail.com>
2017-01-04 06:39:48 +0100

    Thymeleaf tecnology added

--- src/main/java/com/jorge/Microspring1BootLab1Application.java
+++ src/main/java/com/jorge/Microspring1BootLab1Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Microspring1BootLab1Application {
+public class Microspring1BootLab1Application{
 
+	/**
+	 * Used when run as a JAR
+	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(Microspring1BootLab1Application.class, args);
 	}
