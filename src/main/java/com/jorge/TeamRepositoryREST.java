739d09946b6a2f844fbcc444d290ddcd696fa98e
Yorso <jorgeguzon@gmail.com>
2017-01-10 14:58:12 +0100

    Changes

--- src/main/java/com/jorge/TeamRepositoryREST.java
+++ src/main/java/com/jorge/TeamRepositoryREST.java
@@ -8,7 +8,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 //	  http://localhost:8080/teamsREST     =>  Muestra info de todos los equipos
 //	  http://localhost:8080/teamsREST/2   =>  Muestra info del equipo 2
 //    http://localhost:8080/teamsREST/2/players  => Muestra los jugadores del equipo 2
-@RestResource(path="teamsREST", rel="team") 
+@RestResource(path="teamsREST", rel="team") //Si coexisten Spring Data JPA Repositories y Spring Data REST Repositories path debe ser diferencte a cualquier @RequestMapping de cualquier controller
 public interface TeamRepositoryREST extends CrudRepository<Team, Long>{
 	
 }
